91aed514e9718d1bd81fb061555d8296a0cc98b1
nPeeech <33167904+nPeeech@users.noreply.github.com>
2021-01-21 13:14:18 +0000

    実機サーボで速さと滑らかさを調整

--- Pigpio.cpp
+++ Pigpio.cpp
@@ -27,7 +27,7 @@ void Pigpio::move_camera_by_polling()
             pulse = MIN_SERVO_PULSE;
         set_servo_pulsewidth(pi, SERVO_PIN, pulse);
         // printf("パルス幅:%d\n", pulse);
-        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 }
 
