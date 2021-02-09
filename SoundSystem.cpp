b9f7b71431ae353c21b0b900588cba017a041817
Ishii Kouto <takuminn717@gmail.com>
2021-02-09 21:40:08 +0900

    [fix]音声再生中に他の操作ができない

--- SoundSystem.cpp
+++ SoundSystem.cpp
@@ -2,7 +2,7 @@
 
 void SoundSystem::play_music(std::string filename) {
     FILE *fp;
-    std::string message = "mpg321 " + filename;
+    std::string message = "mpg321 " + filename + " &";
     char *cstr = new char[message.size() + 1];
     std::strcpy(cstr, message.c_str());
     fp = popen(cstr, "w");
